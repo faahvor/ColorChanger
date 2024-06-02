2e8810dbc405f66bcf636b4e99fe656e1815ca67
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-06-02 09:54:13 +0100

    color

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const box = document.getElementById("box");
-  const change = document.getElementById("change");
-
-  function randomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  change.addEventListener("click", () => {
-    const newColor = randomColor();
-    box.style.backgroundColor = newColor;
+    const box = document.getElementById("box");
+    const change = document.getElementById("change");
+  
+    const randomColor = () => {
+      const letters = "0123456789ABCDEF";
+      return '#' + Array.from({length: 6}).map(() => letters[Math.floor(Math.random() * 16)]).join('');
+    };
+  
+    change.addEventListener("click", () => {
+      box.style.backgroundColor = randomColor();
+    });
   });
-});
+  
